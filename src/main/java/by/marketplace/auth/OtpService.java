9e1d4152f2a2eadaa400bf571400fe873659424d
v.v.kublitskiy <v.v.kublitskiy@tbank.ru>
2026-08-10 08:35:04 +0300

    implement async send-otp + tests

--- src/main/java/by/marketplace/auth/OtpService.java
+++ src/main/java/by/marketplace/auth/OtpService.java
@@ -3,7 +3,7 @@ package by.marketplace.auth;
 import by.marketplace.auth.dto.AuthResponse;
 import by.marketplace.auth.dto.Channel;
 import by.marketplace.jooq.tables.records.OtpCodesRecord;
-import by.marketplace.notification.NotificationService;
+import by.marketplace.notification.NotificationSender;
 import by.marketplace.shared.exception.AppException;
 import by.marketplace.shared.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -11,13 +11,11 @@ import org.jooq.DSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.webauthn.api.AuthenticatorResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
 import java.time.OffsetDateTime;
-import java.util.Objects;
 import java.util.UUID;
 
 import static by.marketplace.jooq.Tables.OTP_CODES;
@@ -30,13 +28,12 @@ public class OtpService {
     private final DSLContext dsl;
     private final OtpRateLimiter rateLimiter;
     private final PasswordEncoder passwordEncoder;
-    private final NotificationService notificationService;
+    private final NotificationSender notificationService;
     private final UserRepository userRepository;
     private final JwtService jwtService;
 
     private static final int OTP_TTL_MINUTES = 5;
     private static final int MAX_ATTEMPTS = 3;
-
     private static final SecureRandom RANDOM = new SecureRandom();
 
     @Transactional
@@ -44,64 +41,55 @@ public class OtpService {
         rateLimiter.tryConsumer(channel, destination);
 
         String code = String.format("%06d", RANDOM.nextInt(1_000_000));
-        String codeHash = passwordEncoder.encode(code);
 
-        dsl.insertInto(OTP_CODES)
+        Long otpId = dsl.insertInto(OTP_CODES)
                 .set(OTP_CODES.CHANNEL, channel.toString())
                 .set(OTP_CODES.DESTINATION, destination)
-                .set(OTP_CODES.CODE_HASH, codeHash)
+                .set(OTP_CODES.CODE, code)
                 .set(OTP_CODES.EXPIRES_AT, OffsetDateTime.now().plusMinutes(OTP_TTL_MINUTES))
-                .execute();
+                .returning(OTP_CODES.ID)
+                .fetchOne()
+                .getId();
 
+        logger.info("OTP created: id={}, destination={}, channel={}", otpId, destination, channel);
 
-        notificationService.sendOtp(channel, destination, code);
-        logger.info("OTP sent to destination={} via {}", destination, channel);
-
-
+        notificationService.sendOtpAsync(otpId, channel, destination, code);
     }
 
-    // Из за реализации (блокировки forUpdate ) не получится делать инкремент  в другой транзакции с REQUIERS_NEW
-    // получим классический deadlock (Внешняя транзакция пытается сделать UPDATE (хотя она как бы выполнена)
-    // а внутренняя транзакция пытается сделать UPDATE тоже в новой транкзации
-    // Хотя 1ый forUpdate выполняется значительно раньше но лок снимется только после того как транзакция за комичена будет
     @Transactional(noRollbackFor = AppException.class)
     public AuthResponse verifyOtp(Channel channel, String destination, String code) {
         OtpCodesRecord otp = dsl.selectFrom(OTP_CODES)
-                        .where(OTP_CODES.DESTINATION.eq(destination))
-                        .and(OTP_CODES.CONSUMED_AT.isNull())
-                        .and(OTP_CODES.EXPIRES_AT.gt(OffsetDateTime.now()))
-                        .limit(1)
-                        .forUpdate()
-                        .fetchOne();
+                .where(OTP_CODES.DESTINATION.eq(destination))
+                .and(OTP_CODES.CONSUMED_AT.isNull())
+                .and(OTP_CODES.EXPIRES_AT.gt(OffsetDateTime.now()))
+                .limit(1)
+                .forUpdate()
+                .fetchOne();
 
+        if (otp == null) {
+            throw new AppException(ErrorCode.OTP_EXPIRED);
+        }
 
-       if (otp == null) {
-           throw new AppException(ErrorCode.OTP_EXPIRED);
-       }
+        if (otp.getAttempts() >= MAX_ATTEMPTS) {
+            throw new AppException(ErrorCode.OTP_EXPIRED);
+        }
 
+        if (!passwordEncoder.matches(code, otp.getCode())) {
+            dsl.update(OTP_CODES)
+                    .set(OTP_CODES.ATTEMPTS, otp.getAttempts() + 1)
+                    .where(OTP_CODES.ID.eq(otp.getId()))
+                    .execute();
 
-       if (otp.getAttempts() >= MAX_ATTEMPTS) {
-           throw new AppException(ErrorCode.OTP_INVALID);
-       }
+            throw new AppException(ErrorCode.OTP_INVALID);
+        }
 
-       if (!passwordEncoder.matches(code, otp.getCodeHash())) {
-           dsl.update(OTP_CODES)
-                   .set(OTP_CODES.ATTEMPTS, otp.getAttempts() + 1)
-                   .where(OTP_CODES.ID.eq(otp.getId()))
-                   .execute();
-
-           throw new AppException(ErrorCode.OTP_INVALID);
-       }
-
-       dsl.update(OTP_CODES)
-               .set(OTP_CODES.CONSUMED_AT, OffsetDateTime.now())
-               .where(OTP_CODES.ID.eq(otp.getId()))
-               .execute();
+        dsl.update(OTP_CODES)
+                .set(OTP_CODES.CONSUMED_AT, OffsetDateTime.now())
+                .where(OTP_CODES.ID.eq(otp.getId()))
+                .execute();
 
         UUID userId = userRepository.upsertByDestination(channel, destination);
 
-        return jwtService.issueTokens(userId,channel == Channel.EMAIL ? destination : null, "BUYER" );
+        return jwtService.issueTokens(userId, channel == Channel.EMAIL ? destination : null, "BUYER");
     }
-
-
 }
